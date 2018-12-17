400dffcbe5cc1902d796c9e408f372eb1c7d1f44
Toshiya Maki(MBA) <t.maki.0402@gmail.com>
2018-12-17 22:27:54 +0900

    reset everything to start from scratch

--- .vim/template/template.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-#import numpy as np
-#import matplotlib.pyplot as plt
-
-def main():
-
-if __name__ == '__main__':
-    main()
--- etc/init/10_vim_setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo start setup vim
-
-#主に.vimrc で設定したので書くことがなくなった
-
-echo "$(tput setaf 2)vim setup complete ✔︎$(tput sgr0)"
