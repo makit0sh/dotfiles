a648bb9e4df46f4f38c77ea807cd673620d81a80
makit0sh <t.maki.0402@gmail.com>
2019-01-20 06:33:35 +0000

    minor change

--- resources/init/ubuntu-wsl/apt_basic_install.sh
+++ resources/init/ubuntu-wsl/apt_basic_install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PACKAGES="language-pack-ja manpages-ja manpages-ja-dev build-essential cmake python3-dev vim-gtk ctags zsh tmux x11-apps x11-utils x11-xserver-utils dbus-x11 texlive-full"
+PACKAGES="language-pack-ja manpages-ja manpages-ja-dev build-essential cmake python3-dev vim-gtk ctags zsh tmux x11-apps x11-utils x11-xserver-utils dbus-x11 texlive-full zathura"
 
 sudo apt update
 sudo apt upgrade -y
