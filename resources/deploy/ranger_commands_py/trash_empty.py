3443728b6c95adfd962a61691503ac3e8949e152
makit0sh <t.maki.0402@gmail.com>
2019-01-24 20:20:59 +0900

    added ranger trash settings

--- resources/deploy/ranger_commands_py/trash_empty.py
+++ resources/deploy/ranger_commands_py/trash_empty.py
@@ -1,8 +1,18 @@
 class empty(Command):
     """:empty
 
-    Empties the trash directory ~/.Trash
+    Empties the gio trash
     """
 
     def execute(self):
         self.fm.run("gio trash --empty")
+
+class trash(Command):
+    """:trash
+
+    Show the gio trash
+    """
+
+    def execute(self):
+        self.fm.cd("~/.local/share/Trash/files")
+
