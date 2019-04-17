a7df8cd3c663f9d6aa6578350ce504d5e7bcb64d
makit0sh <t.maki.0402@gmail.com>
2019-04-17 17:52:56 +0900

    modified tmux setting for ubuntu-1604-JSK

--- resources/init/ubuntu-1604-JSK/apt_basic_install.sh
+++ resources/init/ubuntu-1604-JSK/apt_basic_install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PACKAGES="curl build-essential git cmake python3-dev vim-gtk ctags zsh tmux zathura libgnome-keyring0 texlive-full nautilus-dropbox gnome-tweak-tool"
+PACKAGES="curl build-essential git cmake python3-dev vim-gtk ctags zsh tmux xsel zathura libgnome-keyring0 texlive-full nautilus-dropbox gnome-tweak-tool"
 
 sudo apt update
 sudo apt upgrade -y
