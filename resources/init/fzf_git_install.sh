22c68d9ba2792ba5cb4c29a59fb040052ac6d890
makit0sh <t.maki.0402@gmail.com>
2019-04-26 14:11:02 +0900

    added fzf and ranger setting for ubuntu

---
+++ resources/init/fzf_git_install.sh
@@ -0,0 +1,3 @@
+echo "installing fzf from github"
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
--- resources/init/ubuntu-1604-JSK/apt_basic_install.sh
+++ resources/init/ubuntu-1604-JSK/apt_basic_install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PACKAGES="curl build-essential git cmake python3-dev vim-gtk ctags zsh tmux xsel zathura libgnome-keyring0 texlive-full nautilus-dropbox gnome-tweak-tool"
+PACKAGES="curl build-essential git cmake python3-dev vim-gtk ctags zsh tmux xsel zathura libgnome-keyring0 texlive-full nautilus-dropbox gnome-tweak-tool ranger w3m lynx highlight atool mediainfo xpdf caca-utils"
 
 sudo apt update
 sudo apt upgrade -y
